39add6078b75e9e5395a21deae6f2b834fabfad3
Abdul Haadi <abdul.806@yahoo.com>
2023-11-19 12:57:04 +0500

    Update MyProfilePage.jsx

--- src/pages/MyProfilePage.jsx
+++ src/pages/MyProfilePage.jsx
@@ -1,22 +1,22 @@
-import React, { useEffect } from 'react'
-import UserProfile from '../components/UserProfile/UserProfile'
-import { useGetMyProductsQuery, useGetMyProfileQuery, useGetUserproductsQuery } from '../redux/apiCalls/apiSlice'
-import { getToken } from '../Token/token'
-import { useParams } from 'react-router-dom'
-import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+// import React, { useEffect } from 'react'
+// import UserProfile from '../components/UserProfile/UserProfile'
+// import { useGetMyProductsQuery, useGetMyProfileQuery, useGetUserproductsQuery } from '../redux/apiCalls/apiSlice'
+// import { getToken } from '../Token/token'
+// import { useParams } from 'react-router-dom'
+// import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 
-// This is a test component, it has no real use in the application
-function MyProfilePage() {
-  const token = getToken()
-  const { user_id } = useParams()
-  const { data: userProducts, isLoading, isFetching, isError } = useGetMyProductsQuery(token)
-  if (!isLoading || !isFetching) return <div className='flex items-center justify-center h-[100vh] w-full'><ClimbingBoxLoader color={"#000"} size={20} aria-label="Loading Spinner" data-testid="loader" /></div>
-  if(isError) return <div>Something went wrong</div>
-  return (
-    <>
-      <UserProfile autherizationRequired={true} userProducts={userProducts} />
-    </>
-  )
-}
+// // This is a test component, it has no real use in the application
+// function MyProfilePage() {
+//   const token = getToken()
+//   const { user_id } = useParams()
+//   const { data: userProducts, isLoading, isFetching, isError } = useGetMyProductsQuery(token)
+//   if (!isLoading || !isFetching) return <div className='flex items-center justify-center h-[100vh] w-full'><ClimbingBoxLoader color={"#000"} size={20} aria-label="Loading Spinner" data-testid="loader" /></div>
+//   if(isError) return <div>Something went wrong</div>
+//   return (
+//     <>
+//       <UserProfile autherizationRequired={true} userProducts={userProducts} />
+//     </>
+//   )
+// }
 
-export default MyProfilePage
+// export default MyProfilePage
