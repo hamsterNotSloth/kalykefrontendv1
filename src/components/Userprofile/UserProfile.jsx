00aa6030390e1cafb6751dd773c1208617d7685a
Abdul Haadi <abdul.806@yahoo.com>
2023-11-19 13:18:48 +0500

    Update UserProfile.jsx

--- src/components/Userprofile/UserProfile.jsx
+++ src/components/Userprofile/UserProfile.jsx
@@ -4,12 +4,11 @@ import UserStatistics from './UserStatistics'
 import {  faUser } from '@fortawesome/free-solid-svg-icons'
 import ProductCard from '../Common/ProductCard'
 
-function UserProfile() {
+function UserProfile({  userProducts }) {
 
   return (
     <div className='max-w-[1200px] flex justify-between mx-auto py-7'>
-      User Card
-{/*       <UserCard  />
+      <UserCard  />
       <div className='w-[100%] ml-[70px]'>
         <UserStatistics count={0} text={"Followers"} icon={faUser} />
         <span className='font-semibold text-[18px] inline-block pb-2'>Modals</span>
@@ -18,7 +17,7 @@ function UserProfile() {
             return <ProductCard key={`Userprofile ${Math.random() * Date.now()}`} item={item} userProducts={userProducts} styling={"w-[230px] h-[300px]"} />
           })}
         </div>
-      </div> */}
+      </div>
     </div>
   )
 }
