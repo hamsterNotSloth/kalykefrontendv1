55f01e62953a992f95ccdc649ec749305f028d59
Yatharth Surana <yashsurana2010@gmail.com>
2023-11-27 00:56:41 +0530

    Update HomePage.jsx
    minor edit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import { useSelector } from 'react-redux'
 
 function HomePage() {
   const {isLoading, isError} = useGetAllProductsQuery({filter: "null", category: "null"})
-  if(isError) return <div className='flex items-center justify-center h-[100vh] w-full'><span>Opps, Something went wrong!</span></div>
+  if(isError) return <div className='flex items-center justify-center h-[100vh] w-full'><span>Oops, something went wrong! We're working to fix this please be back later.</span></div>
   if (isLoading ) return <div className='flex items-center justify-center h-[100vh] w-full'><ClimbingBoxLoader color={"#000"} size={20} aria-label="Loading Spinner" data-testid="loader" /></div>
   return (
     <div>
