f969d8489527129e20a3950c24445007debc8136
Yatharth Surana <yashsurana2010@gmail.com>
2023-11-27 01:38:40 +0530

    Update HeaderLeft.jsx
    minor text edit

--- src/components/Header/HeaderLeft.jsx
+++ src/components/Header/HeaderLeft.jsx
@@ -59,7 +59,7 @@ const HeaderLeft = () => {
                     </li>
                     <li>
                       <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                        Got a suggestion
+                        Got a suggestion?
                       </a>
                     </li>
                   </ul>
