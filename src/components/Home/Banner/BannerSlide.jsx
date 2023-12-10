1bcf363412228be0611092710bcc4d9abd058c0c
Abdul Hadi <abdul.806@yahoo.com>
2023-12-10 16:25:03 +0500

    updates

--- src/components/Home/Banner/BannerSlide.jsx
+++ src/components/Home/Banner/BannerSlide.jsx
@@ -21,19 +21,15 @@ function BannerSlide({ item }) {
   };
   return (
     <div className={`bg-[#fff]  flex-row h-[400px] justify-between rounded-lg md:pl-[80px] overflow-x-hideen flex `} >
-      <div className='max-w-[700px] flex  flex-col justify-center'>
-        <div>
-          <h1 className='text-[34px] font-bold'>Kalyke - The best marketplace for designers, by designers</h1>
-          <span className='text-[20px] text-[#848484] font-bold pb-10'>Get the best deals</span>
-        </div>
+      <div className='max-w-[760px] flex  flex-col justify-center'>
         <div>
           {
             item?.promo == 'model' ?
-              <div className='max-w-[324px]'>
-                <span className='mb-6 block text-[16px] font-semibold'>Featured Product</span>
-                <Link to={`/products/${item?.u_id}`} className='flex flex-col'>
-                  <h4 className='text-[18px] text-[#0026ff] font-semibold'>{item?.productName}</h4>
-                  <h4 className='text-[18px] text-[#000] font-semibold'>{item?.price || "free"}</h4>
+              <div>
+                <span className='block text-[16px] font-semibold'>Featured Product</span>
+                <Link to={`/products/${item?.producId}`} className='flex flex-col'>
+                  <h4 className='text-[#000] text-[30px] w-[100%] font-bold'>{item?.productName}</h4>
+                  <h4 className='text-[18px] text-[#000] font-semibold'>{'$' + item?.price || "free"}</h4>
                 </Link>
                 <div className='max-w-[900px] w-[100%] mt-3' dangerouslySetInnerHTML={{ __html: item?.description }} />
                 <ul className='flex items-end gap-2'>
@@ -47,18 +43,19 @@ function BannerSlide({ item }) {
                     return <SocialMediaRow key={`social-media-share-banner-${Math.random()}-${Date.now()}`} item={item} copyToClipboard={copyToClipboard} />
                   })}
                 </ul>
+                <Link className='flex justify-center items-center mt-4 bg-[#2f85ff] hover:bg-[#5487ff] text-white text-[16px] rounded-md w-[120px] h-[30px]' to={`/products/${item?.producId}`}>View model</Link>
                 {/* <div className='py-3'>
                   <Followbtn productDetails={item} style={'rounded-xl hover:bg-[#e8e8e8] shadow px-4 py-1'} />
                 </div> */}
               </div>
               :
               <div className='max-w-[324px]'>
-                <span className='mb-6 block text-[16px] font-semibold'>Featured Designer</span>
+                <span className='block text-[16px] font-semibold'>Featured Designer</span>
                 <Link to={`/user/${item?.u_id}`} className='flex gap-2 items-center'>
                   <div className='w-[60px] h-[60px]'>
-                    <img src={item.userPicture} className='rounded-full' alt="Profile image" />
+                    <img src={item.userImage} className='rounded-full' alt="Profile image" />
                   </div>
-                  <h4 to={`/user/${item?.u_id}`} className='text-[18px] text-[#0026ff] font-semibold'>{item?.userName}</h4>
+                  <h4 to={`/user/${item?.u_id}`} className='text-[18px] text-[#0026ff] font-semibold'>{item?.displayName}</h4>
                 </Link>
                 <div className='max-w-[900px] w-[100%] mt-3' dangerouslySetInnerHTML={{ __html: item?.description }} />
                 <ul className='flex items-end gap-2'>
@@ -73,14 +70,14 @@ function BannerSlide({ item }) {
                   })}
                 </ul>
                 <div className='py-3'>
-                  <Followbtn productDetails={item} style={'rounded-xl hover:bg-[#e8e8e8] shadow px-4 py-1'} />
+                  <Followbtn productDetails={item} style={'rounded-xl bg-[#2f85ff] hover:bg-[#5487ff] rounded-md text-white shadow px-4 py-1'} />
                 </div>
               </div>
           }
         </div>
       </div>
-      <div className='max-w-[565px] w-full'>
-        <img className='w-[100%] h-[100%] rounded-r-md' src={item?.modelPic} alt="" />
+      <div className='max-w-[700px] w-full'>
+        <img className='w-[100%] h-[100%] rounded-r-md' src={item?.bannerImage} alt="" />
       </div>
     </div>
   )
